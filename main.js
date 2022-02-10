9d2183d28921a92118d072adc966d802b624d672
chy101010 <jason2028823@gmail.com>
2022-02-10 13:09:15 -0500

    complete

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ Modified: 12/06/21
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
+
+const random_div = document.getElementById("random_num");
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let random = Math.random() * 100;
+    random_div.textContent = random;
+}
+
+const circle = document.getElementById("circle");
+function button2Clicked() {
+    if (circle.style.fill === "blue") {
+        circle.style.fill = "green";
+    } else {
+        circle.style.fill = "blue";
+    }
 }
